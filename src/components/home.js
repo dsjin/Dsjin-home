33aadc5f931e7bfac2bf5dcf2b11eb9a2b2f1502
Thatchakon Jom-ud <dsjinj@gmail.com>
2022-11-09 09:31:55 +0900

    fix: layout

--- src/components/home.js
+++ src/components/home.js
@@ -31,26 +31,26 @@ export default function Home() {
         </div>
       </div>
       <div
-        class="flex flex-col justify-around items-center w-full h-screen py-0 md:py-40 px-4"
+        class="flex flex-col justify-around items-center w-full h-screen py-0 lg:py-40 px-4"
       >
         <h1
-          class="md:text-6xl text-4xl"
+          class="lg:text-6xl text-4xl"
         >
           About me
         </h1>
         <div
-          class="flex flex-col md:flex-row items-center justify-center"
+          class="flex flex-col lg:flex-row items-center justify-center"
         >
-          <lottie-player src="https://assets2.lottiefiles.com/packages/lf20_o1JRaPxx1E.json"  background="transparent"  speed="1"  class="md:w-1/5 w-4/5 md:mr-10" loop autoplay />
+          <lottie-player src="https://assets2.lottiefiles.com/packages/lf20_o1JRaPxx1E.json"  background="transparent"  speed="1"  class="lg:w-1/5 w-3/5 lg:mr-10" loop autoplay />
           <p
-            class="md:leading-relaxed md:text-4xl text-2xl indent-12 md:indent-20 md:w-2/5"
+            class="lg:leading-relaxed lg:text-4xl text-2xl indent-12 lg:indent-20 lg:w-2/5"
           >
             An ordinary web full-stack developer who currently lives in Japan (and a DOG person 🐕). I primarily focus on frontend developing but I am also learning about backend and CI/CD stuff.
             I'm a little bit shy but feel free to contact me via these services below.
           </p>
         </div>
         <div
-          class="flex justify-around w-full md:w-2/5"
+          class="flex justify-around w-full lg:w-2/5"
         >
           <SocialIcon url="https://twitter.com/dsjinj" />
           <SocialIcon url="https://github.com/dsjin" bgColor="white" />
